162a15642c8ad25d32e923d454ea72ed182670ba
Malika Arora <malika@vinsol.com>
2019-11-13 18:45:31 +0530

    made same sized buttons in landscape & portrait mode, resolved bugs, improved code

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -25,153 +25,57 @@ class ViewController: UIViewController {
     private var calculationsOn = false
     private var operationTag = 0
     private var equalToOperationFinished = false
-    private var numOfDigitsSoFar = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        guard let text = self.label.text else {return}
-        guard let number = Double(text) else {return}
-        
-        if UIApplication.shared.statusBarOrientation == .portrait {
-            if text.count > 8 {
-                self.displayResultInScientificNotationFor(number: number)
-            }
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        self.selectSameButtonOnOrientationChange()
+        guard let resultString = self.label.text else {return}
+        guard let number = Double(resultString) else {return}
+        if UIDevice.current.orientation.isLandscape {
+            self.label.text = "\(self.previousNumber ?? number)".removeExtraZeroAfterPoint()
         } else {
-            self.label.text = "\(Double(number))".removeExtraZeroAfterPoint()
+            label.text = self.setTextForPortraitMode(resultString: resultString).removeExtraZeroAfterPoint()
         }
     }
 
     // MARK: IBActions
     @IBAction private func numberTapped(_ sender: CustomButton) {
-        if self.equalToOperationFinished {
-            self.checkForNewOrChainCalculation()
+        let digit = sender.tag - 1
+        if self.calculationsOn {
+            self.prepareFirstDigitOfSecondOperand(tag: sender.tag, digit: digit)
+        } else {
+            self.prepareNumber(tag: sender.tag, digit: digit)
         }
-        if calculationsOn {
-            label.text = String(sender.tag-1)
-            currentNumber = Double(label.text!)!
-            for btn in self.btnOperations where btn.tag == self.operationTag {
-                btn.backgroundColor = #colorLiteral(red: 0.7568627451, green: 0.5333333333, blue: 0.2078431373, alpha: 1)
-                btn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
-            }
-            calculationsOn = false
-            self.numOfDigitsSoFar += 1
-        }
-        else { // Number preparation from digits
-            if label.text == "0" {
-                label.text = ""
-            }
-            self.restrictDigitsNum(tag: sender.tag)
-            self.numOfDigitsSoFar += 1
-            currentNumber = Double(label.text ?? "") ?? 0.0
-        }
+        equalToOperationFinished = false
     }
     
     @IBAction private func btnOperationTapped(_ sender: CustomButton) {
-        var previousOperationTag: Int?
-        if self.calculationsOn {
-            previousOperationTag = self.operationTag
-        }
-        if label.text != "" && sender.tag != Operators.Clear.rawValue && sender.tag != Operators.EqualTo.rawValue {
-            self.computePreviousNumAndUpdateOperationOnLabel(tag: sender.tag)
-        }
-        else if sender.tag == Operators.EqualTo.rawValue {
-            self.equalToOperatorTapped()
-            self.numOfDigitsSoFar = 0
-            return
-        }
-        else if sender.tag == Operators.Clear.rawValue {
-            label.text = "0"
-            previousNumber = nil
-            currentNumber = nil
-            operationTag = 0
-        }
-        self.numOfDigitsSoFar = 0
-        sender.backgroundColor = .white
-        sender.setTitleColor(#colorLiteral(red: 0.7568627451, green: 0.5333333333, blue: 0.2078431373, alpha: 1), for: .normal)
-        guard let previousOpTag = previousOperationTag else {return}
-        for btn in self.btnOperations where btn.tag == previousOpTag {
-            btn.backgroundColor = #colorLiteral(red: 0.7568627451, green: 0.5333333333, blue: 0.2078431373, alpha: 1)
-            btn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
-        }
-    }
-    
-    private func restrictDigitsNum(tag: Int) {
-        guard let text = self.label.text else {return}
-        if self.numOfDigitsSoFar <= 7 {
-            if tag == Operators.Dot.rawValue {
-                self.dotOperatorTapped(labelText: text)
-            } else {
-                label.text = text + String(tag-1)
+        if sender.tag == Operators.Clear.rawValue {
+            self.clearOperation()
+        } else if sender.tag == Operators.EqualTo.rawValue {
+            self.equalToOperation()
+        } else { // +,-,/,* operations
+            sender.backgroundColor = .white
+            sender.setTitleColor(#colorLiteral(red: 0.7568627451, green: 0.5333333333, blue: 0.2078431373, alpha: 1), for: .normal)
+            if sender.tag != operationTag {
+                self.setButtonDefaultColor()
             }
+            previousNumber = previousNumber != nil ? computePreviousNumForChainCalculations() : currentNumber
+            operationTag = sender.tag
+            calculationsOn = true
         }
+        currentNumber = nil
+        var resultString = "\(previousNumber ?? 0)".removeExtraZeroAfterPoint()
+        if UIApplication.shared.statusBarOrientation.isPortrait {
+            resultString = self.setTextForPortraitMode(resultString: resultString)
+        }
+        label.text = "\(resultString)".removeExtraZeroAfterPoint()
     }
     
-    private func checkForNewOrChainCalculation() {
-        currentNumber = 0
-        // Operation on previous result as first operand
-        if !self.calculationsOn {//label.text != "÷" && label.text != "*" && label.text != "+" && label.text != "-" {
-            self.operationTag = 0
-            self.previousNumber = nil
-        }
-        self.equalToOperationFinished = false
-        label.text = ""
-    }
-    
-    private func equalToOperatorTapped() {
-        
-        if let secondOperand = self.currentNumber {
-            switch self.operationTag {
-            case Operators.Divide.rawValue:
-                label.text = String(previousNumber! / secondOperand).removeExtraZeroAfterPoint()
-            case Operators.Multiply.rawValue:
-                label.text = String(previousNumber! * secondOperand).removeExtraZeroAfterPoint()
-            case Operators.Subtract.rawValue:
-                label.text = String(previousNumber! - secondOperand).removeExtraZeroAfterPoint()
-            case Operators.Add.rawValue:
-                label.text = String(previousNumber! + secondOperand).removeExtraZeroAfterPoint()
-            default:
-                break
-            }
-            // Result stored for chain calculations
-            self.previousNumber = Double(label.text!) ?? 0.0
-        } else { //incomplete state- both operands are not provided
-            for btn in self.btnOperations where btn.tag == self.operationTag {
-                btn.backgroundColor = #colorLiteral(red: 0.7568627451, green: 0.5333333333, blue: 0.2078431373, alpha: 1)
-                btn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
-            }
-            self.calculationsOn = false
-        }
-        self.currentNumber = nil
-        self.equalToOperationFinished = true
-    }
-    
-    private func computePreviousNumAndUpdateOperationOnLabel(tag: Int) {
-        self.preparePreviousNumForChainCalculations()
-        self.previousNumber = self.previousNumber == nil ? self.currentNumber : self.previousNumber
-        guard let firstOperand = self.previousNumber else {return }
-        switch tag {
-        case Operators.Divide.rawValue:
-            label.text = "\(firstOperand)".removeExtraZeroAfterPoint()
-        case Operators.Multiply.rawValue:
-            label.text = "\(firstOperand)".removeExtraZeroAfterPoint()
-        case Operators.Subtract.rawValue:
-            label.text = "\(firstOperand)".removeExtraZeroAfterPoint()
-        case Operators.Add.rawValue:
-            label.text = "\(firstOperand)".removeExtraZeroAfterPoint()
-        default:
-            print("")
-        }
-        operationTag = tag
-        calculationsOn = true
-        self.currentNumber = nil
-    }
-    
-    private func  preparePreviousNumForChainCalculations() {
-        
+    private func  computePreviousNumForChainCalculations() -> Double {
         // Prepare previousNum by computing previous chain result
         if self.previousNumber != nil && self.currentNumber != nil {
             switch self.operationTag {
@@ -184,45 +88,120 @@ class ViewController: UIViewController {
             case Operators.Add.rawValue:
                 self.previousNumber = previousNumber! + currentNumber!
             default:
-                break
+                previousNumber = currentNumber!
             }
         }
-        guard let text = self.label.text else {return}
-        self.previousNumber = self.previousNumber ?? Double(text)
+        return previousNumber!
     }
     
-    private func displayResultInScientificNotationFor(number: Double) {
+    private func displayResultInScientificNotationFor(number: Double) -> String? {
         let formatter = NumberFormatter()
         formatter.numberStyle = .scientific
-        formatter.positiveFormat = "0.###E+0"
+        formatter.positiveFormat = "0.#####E+0"
         formatter.exponentSymbol = "e"
         if let scientificFormatted = formatter.string(for: number) {
-            self.label.text = scientificFormatted
-            print(scientificFormatted)
+            return scientificFormatted
         }
+        return nil
     }
     
-    private func dotOperatorTapped(labelText: String) {
+    private func dotOperatorTapped() {
+        guard let text = self.label.text else {return}
         // To have only one decimal in the number
-        if !(labelText.contains(".")) {
-            if self.label.text == "" {
+        if !(text.contains(".")) {
+            if text == "" {
                 // To avoid Double(.) -> nil case
                 self.label.text = "0."
             } else {
-                self.label.text = labelText + "."
+                self.label.text = text + "."
             }
+        }
+    }
+    
+    private func removeExtraDigitsAfterPointForPortraitMode(resultString: String) -> String {
+        guard let stringIndex = resultString.firstIndex(of: ".") else {return ""}
+        var resultString = resultString
+        let index = resultString.distance(from: resultString.startIndex, to: stringIndex)
+        if index >= 7 {
+            resultString = (displayResultInScientificNotationFor(number: (previousNumber ?? 0))) ?? ""
+        } else {
+            resultString = String(resultString.prefix(8))
+        }
+        return resultString
+    }
+    
+    private func setTextForPortraitMode(resultString: String) -> String {
+        var resultString = resultString
+        if resultString.count > 8 {
+            if resultString.contains(".") {
+                resultString = self.removeExtraDigitsAfterPointForPortraitMode(resultString: resultString)
+            } else {
+                resultString = (displayResultInScientificNotationFor(number: (previousNumber ?? 0))) ?? ""
+            }
+        }
+        return resultString
+    }
+    
+    private func setButtonDefaultColor() {
+        for btn in self.btnOperations where btn.tag == self.operationTag {
+            btn.backgroundColor = #colorLiteral(red: 0.7568627451, green: 0.5333333333, blue: 0.2078431373, alpha: 1)
+            btn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        }
+    }
+    
+    private func selectSameButtonOnOrientationChange() {
+        for btn in self.btnOperations where btn.tag == self.operationTag {
+            btn.backgroundColor = .white
+            btn.setTitleColor(#colorLiteral(red: 0.7568627451, green: 0.5333333333, blue: 0.2078431373, alpha: 1), for: .normal)
+        }
+    }
+    
+    private func equalToOperation() {
+        previousNumber = previousNumber != nil ? computePreviousNumForChainCalculations() : currentNumber
+        self.setButtonDefaultColor()
+        operationTag = 0
+        equalToOperationFinished = true
+    }
+    
+    private func clearOperation() {
+        self.setButtonDefaultColor()
+        operationTag = 0
+        currentNumber = nil
+        previousNumber = nil
+    }
+    
+    private func prepareFirstDigitOfSecondOperand(tag: Int, digit: Int) {
+        if tag == Operators.Dot.rawValue {
+            label.text = "0."
+        } else {
+            label.text =  "\(digit)"
+        }
+        calculationsOn = false
+        currentNumber = Double(label.text ?? "") ?? 0.0
+        self.setButtonDefaultColor()
+    }
+    
+    private func prepareNumber(tag: Int, digit: Int) {
+        if label.text == "0" { //Remove unneccessary zero at start
+            label.text = ""
+        }
+        guard let text = self.label.text else {return}
+        if text.count <= 7 || equalToOperationFinished {
+            if tag == Operators.Dot.rawValue {
+                label.text = equalToOperationFinished ? "0." : label.text!
+                dotOperatorTapped()
+            } else {
+                label.text = equalToOperationFinished ? "\(digit)" : (label.text! + "\(digit)")
+            }
+            currentNumber = Double(label.text ?? "") ?? 0.0
         }
     }
 }
 
 class CustomButton: UIButton {
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        self.layer.cornerRadius = self.frame.height / 2.0
-    }
-    
     override func awakeFromNib() {
         self.clipsToBounds = true
+        self.layer.cornerRadius = self.frame.height / 2.0
         self.titleLabel!.font = self.titleLabel?.font.withSize(35)
     }
 }
